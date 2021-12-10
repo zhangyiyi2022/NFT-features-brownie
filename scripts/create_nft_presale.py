1633650b46c8331c546be251443e4cd1b768202e
Mikael Aakko <mikael.aakko@gmail.com>
2021-12-10 17:34:34 +0200

    added presale scripts

---
+++ scripts/create_nft_presale.py
@@ -0,0 +1,48 @@
+from brownie import CustomNFT
+from scripts.helper_functions import get_account
+
+# import from file or insert addresses here
+whitelist_addresses = ["0x321312", "0x412526534"]
+
+
+def add_to_whitelist():
+    account = get_account()
+    contract = CustomNFT[len(CustomNFT) - 1]
+
+    add_tx = contract.addToWhiteList(whitelist_addresses, {"from": account})
+    add_tx.wait(1)
+
+    print(f"Added everyone to whitelist succesfully!")
+
+
+def flip_presale_state():
+    account = get_account()
+    contract = CustomNFT[len(CustomNFT) - 1]
+
+    sale_tx = contract.presaleStateChange({"from": account})
+    sale_tx.wait(1)
+
+    print("Presale started!")
+
+
+def create_presale_nft(mint_amount, presale_mint_price):
+
+    fee = presale_mint_price * mint_amount
+
+    account = get_account()
+    contract = CustomNFT[len(CustomNFT) - 1]
+
+    create_tx = contract.createTokenPresale(
+        mint_amount,
+        {"from": account, "value": fee},
+    )
+    create_tx.wait(1)
+
+    print("Collectible created!")
+    print(f"{contract.totalSupply()} total nfts bought!")
+
+
+def main():
+    add_to_whitelist()
+    flip_presale_state()
+    create_presale_nft(1, 88800000000000000)
